3a5a04309d16682939179bf48b70bb89e19eb305
haojie06 <haojie06@qq.com>
2019-10-20 11:25:26 +0800

    add config

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ import config
 if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_builtin_plugins()
-    nonebot.run(host='172.17.0.1', port=8088)
+    nonebot.run()
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ from nonebot.default_config import *
 
 SUPERUSERS = {1092674760}
 COMMAND_START = {'', '/', '!', '／', '！'}
+
+HOST = '172.17.0.1'
+PORT = 8088

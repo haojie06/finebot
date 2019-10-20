8e662dbd2052fae370b4539649c50dbe19ef2462
haojie06 <haojie06@qq.com>
2019-10-20 11:34:47 +0800

    import plugins

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import nonebot
 import config
+from os import path
 
 if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_builtin_plugins()
+    nonebot.load_plugins(
+        path.join(path.dirname(__file__), "fine", "plugins"), "fine.plugins"
+    )
     nonebot.run()
